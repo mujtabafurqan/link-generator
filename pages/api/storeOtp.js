4f6b5fa9bf2d5a3c6b47fa8b0732820dc09d6ca5
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-06 20:08:57 -0800

    returning otp

--- pages/api/storeOtp.js
+++ pages/api/storeOtp.js
@@ -8,5 +8,5 @@ export default async (req, res) => {
     const otp = Math.floor(100000 + Math.random() * 900000);
     const otpRedis = await redis.set(otpId, otp, 'EX', 5*60);
     const link = `http://localhost:3000/getotp/${otpId}`
-    res.status(200).json({ link })
+    res.status(200).json({ link, otp })
 }
