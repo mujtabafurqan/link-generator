bf82815c743320371af077212b1c06d6926f2e91
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-07 17:27:29 -0800

    Ip Adress check

--- pages/api/storeOtp.js
+++ pages/api/storeOtp.js
@@ -4,9 +4,15 @@ import uuid from 'react-uuid';
 let redis = new Redis(process.env.REDIS_URL)
 
 export default async (req, res) => {
+
+    const {
+        query: { ipaddress },
+        method,
+      } = req;
+
     const otpId = uuid();
     const otp = Math.floor(100000 + Math.random() * 900000);
-    const otpRedis = await redis.set(otpId, otp, 'EX', 5*60);
-    const link = `http://localhost:3000/getotp/${otpId}`
+    const otpRedis = await redis.set(otpId, otp + " : " + ipaddress, 'EX', 5*60);
+    const link = `${process.env.NEXTAUTH_URL}/getotp/${otpId}`
     res.status(200).json({ link, otp })
 }
---
+++ pages/api/verifyOtp.js
@@ -0,0 +1,23 @@
+import Redis from 'ioredis'
+
+let redis = new Redis(process.env.REDIS_URL)
+
+export default async (req, res) => {
+
+    const {
+        query: { otpId, otp },
+        method,
+      } = req;
+
+    const otpAndIp = await redis.get(otpId);
+    const otpFromRedis = otpAndIp.split(" : ")[0];
+    const ipFromRedis = otpAndIp.split(" : ")[1];
+    if(otpFromRedis !== otp) {
+        res.status(200).json({ status : 'failure', message: 'OTP does not match' })
+    }
+    if(ipFromRedis !== req.connection.remoteAddress) {
+        res.status(200).json({ status : 'failure', message: 'IP does not match' })
+    }
+    
+    res.status(200).json({ status : 'success'})
+}
