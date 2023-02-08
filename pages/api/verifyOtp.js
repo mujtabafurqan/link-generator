224b3991a1c38488db7bda5afd265c3e6557c8df
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-07 18:15:57 -0800

    Ip Adress check

--- pages/api/verifyOtp.js
+++ pages/api/verifyOtp.js
@@ -20,7 +20,7 @@ export default async (req, res) => {
         if(otpFromRedis !== otp) {
             res.status(200).json({ status : 'failure', message: 'OTP does not match' })
         }
-        else if(ipFromRedis !== req.headers['x-forwarded-for'] || req.connection.remoteAddress) {
+        else if(ipFromRedis !== (req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
             res.status(200).json({ status : 'failure', message: 'IP does not match' })
         }else{
             res.status(200).json({ status : 'success'})
