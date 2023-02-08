c268d155a2034c8c85c7bb40158361eb84dfe566
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-07 18:54:32 -0800

    Ip Adress check

--- pages/api/verifyOtp.js
+++ pages/api/verifyOtp.js
@@ -14,8 +14,9 @@ export default async (req, res) => {
     if(otpAndIp === null) {
         res.status(200).json({ status : 'failure', message: 'OTP expired' })
     }else{
-        const otpFromRedis = otpAndIp.split(" : ")[0];
-        const ipFromRedis = otpAndIp.split(" : ")[1];
+        const otpFromRedis = otpAndIp.split(":")[0];
+        const ipFromRedis = otpAndIp.split(":")[1];
+        console.log(otpFromRedis, otp);
         if(otpFromRedis != otp) {
             res.status(200).json({ status : 'failure', message: 'OTP does not match' })
         }
