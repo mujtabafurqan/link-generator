2664067c785ff1338bc018e23132284d6607c87e
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-11 12:35:34 -0800

    asfaafa

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import styles from "./header.module.css"
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
-export default function Header({show}) {
+export default function Header({show}:{show:boolean}) {
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
