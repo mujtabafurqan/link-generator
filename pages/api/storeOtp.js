69f4adeffe1df5bfd82edf61fdbdbd2cdffffd9d
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2023-02-11 12:24:26 -0800

    asfaafa

--- pages/api/storeOtp.js
+++ pages/api/storeOtp.js
@@ -6,7 +6,7 @@ let redis = new Redis(process.env.REDIS_URL)
 export default async (req, res) => {
 
     const {
-        query: { ipaddress },
+        query: { ipaddress, authorized },
         method,
       } = req;
 
@@ -14,6 +14,10 @@ export default async (req, res) => {
     const otp = Math.floor(100000 + Math.random() * 900000);
     const otpRedis = await redis.set(otpId, otp + ":" + ipaddress, 'EX', 20*60);
     console.log(otpRedis);
-    const link = `${process.env.NEXTAUTH_URL}/getotp/${otpId}`
+    let link;
+    if(authorized)
+      link= `${process.env.NEXTAUTH_URL}/getotp/${otpId}?authorized=true`
+    else
+      link= `${process.env.NEXTAUTH_URL}/getotp/${otpId}?authorized=false`
     res.status(200).json({ link, otp, otpId })
 }
