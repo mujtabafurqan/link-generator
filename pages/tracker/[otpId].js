255cbe93e463c5c5623b27929d36e38838812bc2
mujtabafurqan <mujtaba.1996.mf@gmail.com>
2023-04-09 21:28:17 -0700

    Added Dockerfile

--- pages/tracker/[otpId].js
+++ pages/tracker/[otpId].js
@@ -39,7 +39,7 @@ export async function getServerSideProps(context) {
     if(otpAndIp == null){
         return {
             props: {
-                otp: null,
+                otp: otpFromRedis,
             }
         }
     }else{
