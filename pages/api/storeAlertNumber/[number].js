32f117f1e40ff8bb9592fd6204da1eb7e6d62393
mujtabafurqan <mujtaba.1996.mf@gmail.com>
2023-04-25 03:22:34 -0500

    Send Alerts

--- pages/api/storeAlertNumber/[number].js
+++ pages/api/storeAlertNumber/[number].js
@@ -7,7 +7,11 @@ export default async (req, res) => {
 
     const db = await getDb(dbName);
     const {number} = req.query;
+    let mobileplus = number;
     console.log(" wivfbuwfieuiwfbuiwfebu"+number);
+    if(!number.startsWith('+')){
+        mobileplus = number.replace(" ", "+");
+    }
     if(number.startsWith('+')){
         db.collection('alertNumbers').insertOne({number: number, created_at: new Date()}, async (err, result) => {
             if(err){
@@ -20,11 +24,11 @@ export default async (req, res) => {
                     const status = await client.send({
                         context: "test",
                         from: TOLL_FREE_NUMBER, 
-                        to: number,
+                        to: mobileplus,
                         body: "Welcome to the OTP Cloud Demo. You have been subscribed to receive alerts for the next 15 mins",
                         direction: 'outbound'
                     });
-                    console.log(" New Outbound Message from " + TOLL_FREE_NUMBER + " to " + number + " with status " + status.data);
+                    console.log(" New Outbound Message from " + TOLL_FREE_NUMBER + " to " + mobileplus + " with status " + status.data);
                 } catch (error) {
                   console.log("Error sending message " + error);
                 }
