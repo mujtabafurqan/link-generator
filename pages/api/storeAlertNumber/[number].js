108094394584a6df76c0dc786aad42d76403b395
mujtabafurqan <mujtaba.1996.mf@gmail.com>
2023-04-25 01:26:16 -0500

    Send Alerts

--- pages/api/storeAlertNumber/[number].js
+++ pages/api/storeAlertNumber/[number].js
@@ -1,12 +1,8 @@
 import { getDb } from '../../../lib/mongodb';
-const {Messaging} = require("@signalwire/realtime-api");
+import { getClient } from '../../../lib/signalwire';
 
 const dbName = process.env.WEBAUTHN_DBNAME;
-const client = new Messaging.Client({
-    project: process.env.SIGNALWIRE_PROJECT_ID,
-    token: process.env.SIGNALWIRE_TOKEN,
-    contexts: ["test"],
-  });
+
 export default async (req, res) => {
 
     const db = await getDb(dbName);
@@ -20,7 +16,7 @@ export default async (req, res) => {
                 try {
                     const TOLL_FREE_NUMBER = '+18336529396';
               
-                    const status = await client.send({
+                    const status = await getClient().send({
                         context: "test",
                         from: TOLL_FREE_NUMBER, 
                         to: number,
