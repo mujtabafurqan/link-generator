e025b5f922b8056afb80c30d8adf50531571414b
mujtabafurqan <mujtaba.1996.mf@gmail.com>
2023-05-03 22:46:29 -0700

    asgafag

--- pages/api/verifyOtp.js
+++ pages/api/verifyOtp.js
@@ -2,6 +2,9 @@ import Redis from 'ioredis'
 
 let redis = new Redis(process.env.REDIS_URL)
 
+const sendAlerts = async (mobile) => {
+    await fetch('/api/sendAlerts/'+ mobile)
+}
 export default async (req, res) => {
 
     const {
@@ -15,7 +18,7 @@ export default async (req, res) => {
     if(otpAndIp === null) {
         res.status(200).json({ status : 'failure', message: 'OTP expired' })
     }else if(await redis.get(`${mobileplus}-status`) == 'false'){
-        fetch('/api/sendAlerts/'+mobileplus)
+        sendAlerts(mobileplus)
         res.status(200).json({ status : 'failure', message: 'IP Mismatch. Please request a tracker Link to verify again'})
     }
     else{
@@ -23,11 +26,11 @@ export default async (req, res) => {
         const ipFromRedis = otpAndIp.split(":")[1];
         console.log(otpFromRedis, otp);
         if(otpFromRedis != otp) {
-            fetch('/api/sendAlerts/'+mobileplus)
+            sendAlerts(mobileplus)
             res.status(200).json({ status : 'failure', message: 'OTP does not match' })
         }
         else if(ipFromRedis != 'true') {
-            fetch('/api/sendAlerts/'+mobileplus)
+            sendAlerts(mobileplus)
             res.status(200).json({ status : 'failure', message: 'IP does not match'})
         }else{
             res.status(200).json({ status : 'success'})
